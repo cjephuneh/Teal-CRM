3def5f0b879b4fb17f1019d0e9b0345708734aef
cjephuneh <calebjephuneh@gmail.com>
2023-05-27 08:54:55 +0300

    more--code

--- tealcrm/core/views.py
+++ tealcrm/core/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/index.html')
+
+def about(request):
+    return render(request, 'core/about.html')
