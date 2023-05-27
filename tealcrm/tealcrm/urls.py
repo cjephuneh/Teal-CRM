f92c6a2b91b53e137d2cd0d207802e60b3229bcf
cjephuneh <calebjephuneh@gmail.com>
2023-05-27 11:05:56 +0300

    dashboard

--- tealcrm/tealcrm/urls.py
+++ tealcrm/tealcrm/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core.views import index, about
 from userprofile.views import signup
 from django.contrib.auth import views
@@ -12,6 +12,7 @@ urlpatterns = [
     path('sign-up/', signup, name='signup'),
     path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
     path('log-out/', views.LogoutView.as_view(), name='logout'),
+    path('dashboard/', include('dashboard.urls')),
     path('', index, name='index')
 ]
       
