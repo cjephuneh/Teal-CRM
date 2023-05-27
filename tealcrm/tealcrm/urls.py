1baed2fa3b85fd7368e07a3adddda33ef2494410
cjephuneh <calebjephuneh@gmail.com>
2023-05-27 10:48:38 +0300

    teal-crm-app

--- tealcrm/tealcrm/urls.py
+++ tealcrm/tealcrm/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
     path('sign-up/', signup, name='signup'),
-    path('log-in', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-in/', views.LoginView.as_view(template_name='userprofile/login.html'), name='login'),
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
     path('', index, name='index')
 ]
       
