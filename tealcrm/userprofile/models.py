827aee5ea4fb800a08957c0fa66e51f8a0caf181
cjephuneh <calebjephuneh@gmail.com>
2023-06-01 22:32:09 +0300

    leads

--- tealcrm/userprofile/models.py
+++ tealcrm/userprofile/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class Userprofile(models.Model):
     user= models.OneToOneField(User, related_name='userprofile', on_delete=models.CASCADE)
 
-def __str__(self):
-        return self.Userprofile
+
+
